74e515663a25ad655e0713e9d0d18894d3e67c21
zhliu <zhliu0106@gmail.com>
2022-07-04 15:47:50 +0800

    init

---
+++ README.md
@@ -0,0 +1,39 @@
+# Notion Database Bot
+
+## Prerequisites
+
+1. [Create an integration](https://developers.notion.com/docs/getting-started#step-1-create-an-integration), then you can get your own Internal Integration Token (`token`).
+
+2. [Share a database with your integration](https://developers.notion.com/docs/getting-started#step-2-share-a-database-with-your-integration).
+
+3. Get your target database ID (`database_id`):
+   > Here's a quick procedure to find the database ID for a specific database in Notion:
+   >
+   >> Open the database as a full page in Notion. Use the Share menu to Copy link. Now paste the link in your text editor so you can take a closer look. The URL uses the following format:
+   >>
+   >> https://www.notion.so/{workspace_name}/{database_id}?v={view_id}
+   >> Find the part that corresponds to {database_id} in the URL you pasted. It is a 36 character long string. This value is your database ID.
+   >> Note that when you receive the database ID from the API, e.g. the search endpoint, it will contain hyphens in the UUIDv4 format. You may use either the hyphenated or un-hyphenated ID when calling the API.
+
+## Usage
+
+- Init your Bot:
+  ```python
+  bot = NotionDatabaseBot(token)
+  ```
+
+- Read database content:
+  ```python
+  content = bot.read_database(database_id)
+  ```
+
+- Add a page/row/sample to database
+  ```python
+  temp_dict = {"Model": "bert-base-uncased", "ppl": "5.21"}
+  properties = bot.dict2properties(temp_dict)
+  bot.create_page(database_id, properties)
+  ```
+
+### References:
+- https://developers.notion.com/
+- https://prettystatic.com/notion-api-python/
---
+++ main.py
@@ -0,0 +1,108 @@
+# Based on https://prettystatic.com/notion-api-python/
+
+import requests, json
+
+
+class NotionDatabaseBot:
+    """"""
+
+    def __init__(self, token: str):
+        """
+        Init an Notion Database Bot object.
+
+        Parameters:
+            - token: Internal Integration Token
+        """
+        self.token = token
+        self.headers = {"Authorization": "Bearer " + token, "Content-Type": "application/json", "Notion-Version": "2022-02-22"}
+
+    def read_database(self, database_id: str):
+        """
+        Read Notion Database content.
+
+        Parameters:
+            - database_id: Notion Database ID
+        """
+        readUrl = f"https://api.notion.com/v1/databases/{database_id}/query"
+
+        res = requests.request("POST", readUrl, headers=self.headers)
+        data = res.json()
+        print(res.status_code)
+        print(res.text)
+
+        return data
+
+    def create_page(self, database_id, properties):
+        """
+        Create a new page/row/sample in Notion Database.
+
+        Parameters:
+            - database_id: Notion Database ID
+            - properties: Notion Database Property object (dict), refer to https://developers.notion.com/reference/property-object
+        """
+        createUrl = "https://api.notion.com/v1/pages"
+
+        newPageData = {
+            "parent": {"database_id": database_id},
+            "properties": properties,
+        }
+
+        data = json.dumps(newPageData)
+
+        res = requests.request("POST", createUrl, headers=self.headers, data=data)
+
+        print(res.status_code)
+        print(res.text)
+
+    def update_page(self, page_id, properties):
+        """
+        Update page/row/sample content stored in Notion Database.
+
+        Parameters:
+            - page_id: Notion Page ID
+            - properties: Notion Database Property object (dict), refer to https://developers.notion.com/reference/property-object
+        """
+        update_url = f"https://api.notion.com/v1/pages/{page_id}"
+
+        update_data = {"properties": properties}
+
+        data = json.dumps(update_data)
+
+        response = requests.request("PATCH", update_url, headers=self.headers, data=data)
+
+        print(response.status_code)
+        print(response.text)
+
+    @staticmethod
+    def dict2properties(src_dict: dict, title_key: str = None):
+        """
+        Turn a dict to Property object.
+
+        Parameters:
+            - src_dict: Source dict, contains a title key/value pair and other properties key/value pairs.
+            - title_key: Title key string.
+        """
+        assert title_key is not None, "title_key must be provided."
+        properties = dict()
+        for key, value in src_dict.items():
+            if key == title_key:
+                properties[key] = {"title": [{"text": {"content": str(value)}}]}
+            else:
+                properties[key] = {"rich_text": [{"text": {"content": str(value)}}]}
+        return properties
+
+
+if __name__ == "__main__":
+
+    # Init your Bot
+    token = ""
+    bot = NotionDatabaseBot(token)
+
+    # Read database content
+    database_id = ""
+    content = bot.read_database(database_id)
+
+    # Add a page/row/sample to database
+    temp_dict = {"Model": "bert-base-uncased", "ppl": "5.21"}
+    properties = bot.dict2properties(temp_dict)
+    bot.create_page(database_id, properties)
